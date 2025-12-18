e2aafae42fef7f424c3c12064ed84f09e8b85810
mohammad-abdul-rehman0198 <mohammad.abdul@mergestack.com>
2025-12-18 17:42:08 +0500

    Remove react query and APIs. Add SSR Actions

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,26 +1,10 @@
-import { useEffect, useState } from "react";
-
 import type { Todo } from "@/utils/interfaces/Todo";
 import { todoController } from "@/state/controller/todo";
 
 
 const HeroSection = () => {
   const { todos } = todoController.useState(["todos"]);
-
-  const [todosSize, setTodosSize] = useState<number>(0);
-  const [completedSize, setCompletedSize] = useState<number>(0);
-
-  useEffect(() => {
-    const fetchTodosSize = () => {
-      const completed = todos?.filter((todo: Todo) => todo.status === true);
-
-      setTodosSize(todos?.length || 0);
-      setCompletedSize(completed?.length || 0);
-    };
-
-    fetchTodosSize();
-
-  }, [todos]);
+  const completedSize = todos?.filter((todo: Todo) => todo.status === true).length || 0;
 
   return (
     <section className="w-[70%] max-[510px]:w-[90%] flex justify-around items-center border border-[#c2b39a] rounded-[11px] gap-3 p-3 max-w-[455px]">
@@ -30,7 +14,7 @@ const HeroSection = () => {
       </div>
 
       <div className="bg-[#88ab33] w-[150px] h-[150px] rounded-full text-[48px] flex items-center justify-center">
-        {completedSize}/{todosSize}
+        {completedSize}/{todos?.length || 0}
       </div>
     </section>
   );
