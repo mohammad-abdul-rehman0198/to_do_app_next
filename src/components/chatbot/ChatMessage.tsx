13044a5a8a45d5e377d3813c945a718beca0472e
mohammad-abdul-rehman0198 <mohammad.abdul@mergestack.com>
2025-12-31 13:22:31 +0500

    Add ReatcMarkdown for chat response UI good

--- src/components/chatbot/ChatMessage.tsx
+++ src/components/chatbot/ChatMessage.tsx
@@ -6,7 +6,7 @@ import { Message } from "@/utils/interfaces/Message";
 export const ChatMessage = ({ sender, text }: Message) => {
   return (
     <div
-      className={`p-3 rounded-xl max-w-[75%] wrap-break-words shadow whitespace-pre-wrap ${
+      className={`p-3 rounded-xl max-w-[75%] wrap-break-words shadow my-2 ${
         sender === "user"
           ? "bg-[#88ab33] text-white self-end rounded-br-none"
           : "bg-gray-800 text-gray-200 self-start rounded-bl-none"
